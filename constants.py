cd3ebf89a33fd6a117192701756c8bf68549b496
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-03 23:40:55 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,32 @@
+# # # Constants # # #
+
+
+sunAbsMag = 4.8
+sunAppMag = -26.7
+sunTemp = 5800 #K
+sunCoronaTemp = 1000000 #K
+sunChromTemp = 10000 #K
+sunPhotoTemp = 6000 #K
+sunCoreTemp = 15000000 #K
+sunMass = 1989000000000000000000000000000 #kg
+sunFlux = 1370 #W/m^2
+sunRadius = 695590000 #m
+sunDensity = 1.41 #g/cm^3
+sunEscapeSpeed = 618 #km/s
+sunSurfGrav = 27.9 #times Earth's
+sunEqRotPeriod = 25.04 #days
+sunPoleRotPeriod = 30 #days
+sunAge = 4570000000 #years
+sunOblique = 7.25 #degrees
+
+earthMass = 5980000000000000000000000 #kg
+earthDensity = 5515 #kg/m^3
+earthSurfTemp = 287 #K
+earthSurfGrav = 9.78 #m/s^2
+earthRadius = 6378 #km
+earthMoonDist = 384400 #km
+
+constantStefan = 5.670373*(10**-8) #W(m^-2)(K^-4)
+
+
+#Milankovitch cycles - don't know if we need these
