261426404bd335b9078e42231849a565760973ad
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-04 01:00:59 -0700

    Update module.py
    - Added catalog
    - Replaced 'default' with 'solve'
    - Added Wien's Law
    - Added Luminosity <==> Apparent brightness

--- module.py
+++ module.py
@@ -8,7 +8,6 @@ import constants as const
 # You can do functions within functions, a la C2K(K2F(n)).
 # 3 is cursed. Use int(pi). (http://xkcd.com/1275/)
 
-# # # We should find and replace 'default' with 'solve'. What do you think? -A # # #
 
 '''
 # # # Catalog # # #
@@ -21,15 +20,15 @@ Luminosity & app brightness
 '''
 
 # Kepler's Law (p^2 = a^3)
-def kepler3(a='default',P='default',work='pls'):
-    if a == 'default' and P!='default':
+def kepler3(a='solve',P='solve',work='pls'):
+    if a == 'solve' and P!='solve':
         a = P ** (0.66666666666666666)
         print('Orbital radius A = ',a)
         if work == 'pls':
             print("""P^2 = a^3
                     a = P^(2/3)
                     a =""",a)
-    elif P == 'default' and a!='default':
+    elif P == 'solve' and a!='solve':
         P = a ** (1.5)
         print('Period = ',P)
         if work == 'pls':
@@ -40,20 +39,20 @@ def kepler3(a='default',P='default',work='pls'):
         print('Syntax:  kepler3(a, p, work)') # Maybe information about the formula can go here.
         
 # Distance Modulus   
-def modulus(d='default', appmag='default', absmag='default', work='yas')
-    if d == 'default' and appmag!='default' and absmag!='default':
+def modulus(d='solve', appmag='solve', absmag='solve', work='yas')
+    if d == 'solve' and appmag!='solve' and absmag!='solve':
         d = 10 ** (((appmag-absmag)/5)+1)
         print('Distance d in pc = ', d)
         if work == 'yas':
             print("""d = 10^(((m-M)/5)+1)
                     d = """, d)
-    elif appmag=='default' and d!='default' and absmag!='default':
+    elif appmag=='solve' and d!='solve' and absmag!='solve':
         appmag = 5*log(d/10, 10) + absmag
         print('Apparent magnitude m = ', appmag)
         if work == 'yas':
             print("""m = 5log(0.1d)+M
                     m = """, appmag)
-    elif absmag == 'default' and appmag!='default' and d!='default':
+    elif absmag == 'solve' and appmag!='solve' and d!='solve':
         absmag = -5*log(d/10, 10) + appmag
         print('Absolute magnitude M = ', absmag)
         if work == 'yas':
@@ -64,8 +63,8 @@ def modulus(d='default', appmag='default', absmag='default', work='yas')
 
 # Temperature of a planet
 # Process: To find the expected temperature of a planet, you need to set the rate of energy radiated by the planet equal to the rate of energy absorbed by the planet. 
-def tempOfPlanet(T='default', L='default', a='default', d='default', work='ofc')
-    if T=='default' and L!='default' and a!='default' and d!='default'
+def tempOfPlanet(T='solve', L='solve', a='solve', d='solve', work='ofc')
+    if T=='solve' and L!='solve' and a!='solve' and d!='solve'
         fourthroot = (L*(1-a))/(16*const.constantStefan*pi)
         T = (fourthroot**0.25)*(d**-0.5)
         print('Temperature T in K = ', T)
@@ -75,15 +74,15 @@ def tempOfPlanet(T='default', L='default', a='default', d='default', work='ofc')
             print("""4(pi)(R^2)(sigma)(T^4) = (pi)(R^2)(Lsun)(1-a)/(4(pi)(d^2))
                     T = ((Lsun(1-a)/16(sigma)(pi))^0.25) * (d^-0.5)
                     T = """, T)
-    elif L=='default' and T!='default' and a!='default' and d!='default'
+    elif L=='solve' and T!='solve' and a!='solve' and d!='solve'
         #I don't feel like doing the algebra here...
         if work == 'ofc':
             #print work
-    elif a=='default' and L!='default' and T!='default' and d!='default'
+    elif a=='solve' and L!='solve' and T!='solve' and d!='solve'
         #Much algebra very tired
         if work == 'ofc':
             #print work
-    elif d=='default' and L!='default' and a!='default' and T!='default'
+    elif d=='solve' and L!='solve' and a!='solve' and T!='solve'
         #Lack of paper nearby
         if work == 'ofc':
             #print work
@@ -91,18 +90,18 @@ def tempOfPlanet(T='default', L='default', a='default', d='default', work='ofc')
         print('Syntax:  tempOfPlant(temperature, luminosity of star, albedo, distance from star, work)') # Maybe information about the formula can go here.
 
 # Escape velocity
-def escapeVelocity(v='default', M='default', r='default', work ='mhm')
-    if v=='default' and M!='default' and r!='default
+def escapeVelocity(v='solve', M='solve', r='solve', work ='mhm')
+    if v=='solve' and M!='solve' and r!='default
         v = sqrt((2*const.bigG)/r)
         print('Escape velocity v in m/s = ', v)
         if work == 'mhm'
             print("""v = sqrt(2GM/r)
                     v = """, v)
-    elif M=='default' and v!='default' and r!='default
+    elif M=='solve' and v!='solve' and r!='default
         # Lazy again to solve for mass
         if work == 'mhm'
             #print work
-    elif r=='default' and v!='default' and M!='default
+    elif r=='solve' and v!='solve' and M!='default
         # 2 algebra 2 function
         if work == 'mhm'
             #print work
@@ -110,14 +109,14 @@ def escapeVelocity(v='default', M='default', r='default', work ='mhm')
         print('Syntax:  escapeVeloity(velocity, mass, radius, work)')   
 
 # Wien's Law
-def wien(w='default', T='default', work='yes')
-    if w=='default' and T!='default'
+def wien(w='solve', T='solve', work='yes')
+    if w=='solve' and T!='solve'
         w = const.wien/T
         print('Max wavelength lambda in m = ', w)
         if work =='yes'
             print("""lambda = c/T
                     lambda = """, w)
-    elif T=='default' and w!=default
+    elif T=='solve' and w!=default
         T = const.wien/w
         print('Temperature T in K = ', T)
         if work == 'yes'
@@ -128,21 +127,21 @@ def wien(w='default', T='default', work='yes')
         print('Syntax:  wien(max_wavelength, temperature, work)')
         
 # Luminosity <==> Apparent Brightness
-def lumAppBright(b='default', L='default', d='default', work='hmu')
-    if b=='default' and L!='default' and d!='default'
+def lumAppBright(b='solve', L='solve', d='solve', work='hmu')
+    if b=='solve' and L!='solve' and d!='solve'
         b = L/(4*pi*(d**2))
         print('Apparent brightness b in W/m^2 = ', b)
         if work == 'hmu'
             print("""b = L/4*pi*d^2
                     b = """, b)
-    elif L=='default' and b!='default' and d!='default'
+    elif L=='solve' and b!='solve' and d!='solve'
         L = 4*pi*b*(d**2)
         print('Luminosity L in W = ', L)
         if work == 'hmu'
             print("""b = L/4*pi*d^2
                     L = 4(pi)(b)(d^2)
                     L = """, L)
-    elif d=='default' and b!=default and L!='default'
+    elif d=='solve' and b!='solve' and L!='solve'
         d = sqrt(L/(4*pi*b))
         print('Distance d in m = ', d)
         if work == 'hmu'
