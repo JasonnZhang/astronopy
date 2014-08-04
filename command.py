856d165089bcac34f84e9a86b01b1d4a685c0041
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-04 12:15:50 -0700

    Update command.py

--- command.py
+++ command.py
@@ -1,10 +1,13 @@
-def chdirimp(url='C:/'):
+#Run this file
+
+def chdirimp(addr='C:/'):
 	import os
-	os.chdir(url)
+	os.chdir(addr)
 	import module as astro
 	import convert as conv
 	import const as const
 	dir(astro)
 	dir(conv)
 	dir(const)
-	
+
+#Here, run chdirimp(addr_of_astronopy) to change directory, import astro modules, and list functions
