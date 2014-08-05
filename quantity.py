6d053d41bd6a016207a0320a60f9f6cab776cbca
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 11:33:13 -0700

    Update quantity.py

--- quantity.py
+++ quantity.py
@@ -3,8 +3,8 @@ import numpy as np
 class Quantity(object):
   def __init__(self, value, numUnits, denUnits):
     self.value = value # numerical value
-    self.numUnits = numUnits # array of units in the numerator, i.e. {units.kg, units.m, units.m}
-    self.denUnits = denUnits # array of units in the denominator, i.e. {units.s, units.s}
+    self.numUnits = numUnits # array of units in the numerator, i.e. {kg.abbr, m.abbr, m.abbr}
+    self.denUnits = denUnits # array of units in the denominator, i.e. {s.abbr, s.abbr}
   def put(self):
     strNumUnits = ''
     strDenUnits = ''
@@ -14,4 +14,11 @@ class Quantity(object):
       strDenUnits += item
       # should probably add something here to concatenate like units (instead of something like 'mm')
     print "%s  %s/%s" % (self.value, self.numUnits, self.denUnits)
+
+class Units(object):
+  def __init__(self, abbr, name, category):
+    self.abbr = abbr # unit abbreviation (i.e. 'm')
+    self.name = name # unit name (i.e. 'meters')
+    self.category = category # unit category (i.e. 'length')
+
 
