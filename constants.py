de779ab3bca27040ba638cadb22b44399156ac61
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 12:48:49 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,12 +1,23 @@
 import * units
 
+bigG = Quantity(6.67384*(10**-11), [m.abbr, m.abbr, m.abbr], [kg.abbr, s.abbr, s.abbr]) #(m^3)/(kg(s^2))
+c = Quantity(299792458, [m.abbr], [s.abbr]) #m/s
+
+stefan = (5.670373*(10**-8) #W(m^-2)(K^-4)
+wien = 0.0028977721 #m K
+hubble = 67.8 #(km/s)/Mpc
+
+
+
+
+
+'''
 # # # Constants # # #
 
 sun = dict(absMag=4.8,appMag=-26.7,temp=5800,coronaTemp=1000000)
 
 #All temperatures in K
 #All masses in kg
-'''
 sunAbsMag = 4.8
 sunAppMag = -26.7
 sunTemp = 5800 #K
@@ -33,13 +44,7 @@ earthRadius = 6378 #km
 earthMoonDist = 384400 #km
 '''
 
-stefan = (5.670373*(10**-8) #W(m^-2)(K^-4)
-wien = 0.0028977721 #m K
-hubble = 67.8 #(km/s)/Mpc
 
-
-bigG = Quantity(6.67384*(10**-11), [m.abbr, m.abbr, m.abbr], [kg.abbr, s.abbr, s.abbr]) #(m^3)/(kg(s^2))
-c = Quantity(299792458, [m.abbr], [s.abbr]) #m/s
 
 
 #Milankovitch cycles - don't know if we need these
