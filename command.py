a465069932f0558abcf87af41a21c75718d9dce9
Pluvial <clzhu@ucsd.edu>
2014-08-04 22:14:13 -0700

    Update command.py
    Tweaked import names, added print statements

--- command.py
+++ command.py
@@ -3,11 +3,12 @@
 def chdirimp(addr='C:/'):
 	import os
 	os.chdir(addr)
-	import module as astro
-	import convert as conv
-	import const as const
-	dir(astro)
-	dir(conv)
-	dir(const)
+	import astro as ac #AstroCalc? Can't use 'as'
+	import convert as cv
+	import constant as cs
+	
+	print dir(ac)
+	print dir(cv)
+	print dir(cs)
 
 #Here, run chdirimp(addr_of_astronopy) to change directory, import astro modules, and list functions
