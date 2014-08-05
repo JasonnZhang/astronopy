0218e77cc87d6ff44f232ef1cb15b749443a8cc9
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 11:26:26 -0700

    Update quantity.py

--- quantity.py
+++ quantity.py
@@ -2,9 +2,16 @@ import numpy as np
 
 class Quantity(object):
   def __init__(self, value, numUnits, denUnits):
-    self.value = value
-    self.numUnits = numUnits
-    self.denUnits = denUnits
+    self.value = value # numerical value
+    self.numUnits = numUnits # array of units in the numerator, i.e. {units.kg, units.m, units.m}
+    self.denUnits = denUnits # array of units in the denominator, i.e. {units.s, units.s}
   def put(self):
+    strNumUnits = ''
+    strDenUnits = ''
+    for item in numUnits:
+      strNumUnits += item
+    for item in denUnits:
+      strDenUnits += item
+      # should probably add something here to concatenate like units (instead of something like 'mm')
     print "%s  %s/%s" % (self.value, self.numUnits, self.denUnits)
-  
+
