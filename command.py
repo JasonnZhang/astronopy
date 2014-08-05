7875e375e8e6833d9f31089efef77e5fcd103b22
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 12:47:05 -0700

    Update command.py

--- command.py
+++ command.py
@@ -3,7 +3,7 @@
 def chdirimp(addr='C:/'):
 	import os
 	os.chdir(addr)
-	import astro as ac #AstroCalc? Can't use 'as'
+	import astro as ap # AstronoPy? Can't use 'as'
 	import convert as cv
 	import constant as cs
 	
