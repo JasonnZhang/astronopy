0bb45f54d4aaa60a15c52d9d158f2cfc24671f30
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 11:46:54 -0700

    Create units.py

---
+++ units.py
@@ -0,0 +1,5 @@
+class Units(object):
+  def __init__(self, abbr, name, category):
+    self.abbr = abbr # unit abbreviation (i.e. 'm')
+    self.name = name # unit name (i.e. 'meters')
+    self.category = category # unit category (i.e. 'length')
