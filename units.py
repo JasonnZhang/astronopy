77a3ca56ffcdaebef06de6aeeda2c803a4791e31
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-05 11:52:46 -0700

    Update units.py

--- units.py
+++ units.py
@@ -3,3 +3,22 @@ class Units(object):
     self.abbr = abbr # unit abbreviation (i.e. 'm')
     self.name = name # unit name (i.e. 'meters')
     self.category = category # unit category (i.e. 'length')
+
+def initUnits():
+  # # # Mass # # #
+  kg = Units("kg", "kilograms", "mass")
+  g = Units("g", "grams", "mass")
+  msun = Units("msun", "solar mass", "mass")
+  mearth = Units("mearth", "earth mass", "mass")
+  
+  # # # Length # # #
+  km = Units("km", "kilometers", "length")
+  m = Units("m", "meters", "length")
+  AU = Units("AU", "astronomical units", "length")
+  pc = Units("pc", "parsecs", "length")
+  ly = Units("ly", "light-years", "length")
+  
+  # # # Time # # #
+  s = Units("s", "seconds", "time")
+  hrs = Units("hrs", "hours", "time")
+  days = Units("days", "days", "time")
