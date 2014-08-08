e9adb616ce5f60d76fbc2973359f96498a9d0697
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-07 20:03:25 -0700

    Fixed constants as members of Quantity class

--- astro.py
+++ astro.py
@@ -164,32 +164,32 @@ mag = Units("mag", "magnitude", magDict)
 # This is really helpful: http://physics.nist.gov/cuu/Units/units.html
 
 # # # General # # #
-bigG = Quantity(6.67384*(10**-11), [m.abbr, m.abbr, m.abbr], [kg.abbr, s.abbr, s.abbr]) #(m^3)/(kg(s^2))
-c = Quantity(299792458, [m.abbr], [s.abbr]) #m/s
-hubble = Quantity(67.8/1000000, [km.abbr], [s.abbr, pc.abbr]) #(km/(s*pc))
-wien = Quantity(0.0028977721, [m.abbr, K.abbr], []) #m K
-stefan = Quantity((5.670373*(10**-8)), [kg.abbr], [s.abbr, s.abbr, s.abbr, K.abbr, K.abbr, K.abbr, K.abbr]) #W(m^-2)(K^-4) (converted to base SI units)
+bigG = Quantity(6.67384*(10**-11), [m, m, m], [kg, s, s]) #(m^3)/(kg(s^2))
+c = Quantity(299792458, [m], [s]) #m/s
+hubble = Quantity(67.8/1000000, [km], [s, pc]) #(km/(s*pc))
+wien = Quantity(0.0028977721, [m, K], []) #m K
+stefan = Quantity((5.670373*(10**-8)), [kg], [s, s, s, K, K, K, K]) #W(m^-2)(K^-4) (converted to base SI units)
 
 # # # Sun # # #
-sunCoronaTemp = Quantity(1000000, [K.abbr], []) #K
-sunChromTemp = Quantity(10000, [K.abbr], []) #K
-sunPhotoTemp = Quantity(6000, [K.abbr], []) #K
-sunCoreTemp = Quantity(15000000, [K.abbr], []) #K
-sunMass = Quantity(1989000000000000000000000000000, [kg.abbr], []) #kg
-sunRadius = Quantity(695590000, [m.abbr], []) #m
-# sunDensity = Quantity(1.41, [g.abbr], [cm.abbr, cm.abbr, cm.abbr]) #g/cm^3
-sunAbsMag = Quantity(4.8, [mag.abbr], [])
-sunAppMag = Quantity(-26.7, [mag.abbr], [])
-sunEqRotPeriod = Quantity(25.04, [days.abbr], []) #days
-sunPoleRotPeriod = Quantity(30, [days.abbr], []) #days
+sunCoronaTemp = Quantity(1000000, [K], []) #K
+sunChromTemp = Quantity(10000, [K], []) #K
+sunPhotoTemp = Quantity(6000, [K], []) #K
+sunCoreTemp = Quantity(15000000, [K], []) #K
+sunMass = Quantity(1989000000000000000000000000000, [kg], []) #kg
+sunRadius = Quantity(695590000, [m], []) #m
+# sunDensity = Quantity(1.41, [g], [cm, cm, cm]) #g/cm^3
+sunAbsMag = Quantity(4.8, [mag], [])
+sunAppMag = Quantity(-26.7, [mag], [])
+sunEqRotPeriod = Quantity(25.04, [days], []) #days
+sunPoleRotPeriod = Quantity(30, [days], []) #days
 
 # # # Earth # # #
-earthMass = Quantity(5976*(10**24), [kg.abbr], []) #kg
-earthDensity = Quantity(5515, [kg.abbr], [m.abbr, m.abbr, m.abbr]) #kg/m^3
-earthSurfTemp = Quantity(287, [K.abbr], []) #K
-earthSurfGrav = Quantity(9.78, [m.abbr], [s.abbr, s.abbr]) #m/s^2
-earthRadius = Quantity(6378, [km.abbr], []) #km
-earthMoonDist = Quantity(384400, [km.abbr], []) #km
+earthMass = Quantity(5976*(10**24), [kg], []) #kg
+earthDensity = Quantity(5515, [kg], [m, m, m]) #kg/m^3
+earthSurfTemp = Quantity(287, [K], []) #K
+earthSurfGrav = Quantity(9.78, [m], [s, s]) #m/s^2
+earthRadius = Quantity(6378, [km], []) #km
+earthMoonDist = Quantity(384400, [km], []) #km
 
 
 '''
