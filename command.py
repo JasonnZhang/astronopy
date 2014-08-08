8582d17a2d7f5a80de160ff77ffa190face29c5a
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-07 19:30:31 -0700

    Update command.py

--- command.py
+++ command.py
@@ -5,13 +5,7 @@ def chdirimp(addr='C:/'):
 	
 	os.chdir(addr)
 	import astro as ap # AstronoPy? Can't use 'as'
-	import convert as cv
-	import constant as cs
-	from units import *
-	from quantity import *
+	print dir(ap)
 	
-	print dir(ac)
-	print dir(cv)
-	print dir(cs)
 
 #Here, run chdirimp(addr_of_astronopy) to change directory, import astro modules, and list functions
