6ccc2946202ee84304ac71b0cab53fa3751dc2ea
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-07 17:20:07 -0700

    Update units.py

--- units.py
+++ units.py
@@ -13,62 +13,62 @@ class Units(object):
 
 def initUnits():
   # # # Mass # # #
-  kg = Units("kg", "kilograms", kgDict)
   kgDict = {
     kg: 1, 
     g: 1000, 
     msun: 1/1989000000000000000000000000000, 
     mearth: 1/(5976*(10**24))
     }
+  kg = Units("kg", "kilograms", kgDict)
   
-  g = Units("g", "grams", gDict)
   gDict = {
     kg: 1/kgDict.get(g), 
     g: 1, 
     msun: gDict.get(kg)*kgDict.get(msun),
     mearth: gDict.get(kg)*kgDict.get(mearth)
     }
+  g = Units("g", "grams", gDict)
   
-  msun = Units("msun", "solar mass", msunDict)
   msunDict = {
     kg: 1/kgDict.get(msun), 
     g: 1/gDict.get(msun), 
     msun: 1,
     mearth: msunDict.get(kg)*kgDict.get(mearth)
     }
+  msun = Units("msun", "solar mass", msunDict)
+
   
-  mearth = Units("mearth", "earth mass", mearthDict)
   mearthDict = {
     kg: 1/kgDict.get(mearth), 
     g: 1/gDict.get(mearth), 
     msun: 1/msunDict.get(mearth),
     mearth: 1
     }
+  mearth = Units("mearth", "earth mass", mearthDict)
   
   # # # Time # # #
-  s = Units("s", "seconds", sDict)
   sDict = {
     s: 1, 
     hrs: (1/3600), 
     days: (1/86400)
     }
+  s = Units("s", "seconds", sDict)
     
-  hrs = Units("hrs", "hours", hrsDict)
   hrsDict = {
     s: 3600, 
     hrs: 1, 
     days: 24
     }
+  hrs = Units("hrs", "hours", hrsDict)
   
-  days = Units("days", "days", daysDict)
   daysDict = {
     s: 86400, 
     hrs: 24, 
     days: 1
     }
+  days = Units("days", "days", daysDict)
   
   # # # Length # # #
-  km = Units("km", "kilometers", kmDict)
   kmDict = {
     km: 1,
     m: 1000,
@@ -76,8 +76,8 @@ def initUnits():
     pc: 1/(3.08567758*(10**13)),
     ly: 1/(9.4605284*(10**12))
     }
+  km = Units("km", "kilometers", kmDict)
   
-  m = Units("m", "meters", mDict)
   mDict = {
     km: 1/1000,
     m: 1,
@@ -85,8 +85,8 @@ def initUnits():
     pc: (1/1000)(1/(3.08567758*(10**13))),
     ly: (1/1000)(1/(9.4605284*(10**12)))
     }
+  m = Units("m", "meters", mDict)
   
-  AU = Units("AU", "astronomical units", AUDict)
   AUDict = {
     km: 1/kmDict.get(AU),
     m: 1000/kmDict.get(AU),
@@ -94,8 +94,8 @@ def initUnits():
     pc: 1/206264.806,
     ly: 1/63239.7263
     }
-  
-  pc = Units("pc", "parsecs", pcDict)
+  AU = Units("AU", "astronomical units", AUDict)
+
   pcDict = {
     km: 1/kmDict.get(pc),
     m: 1/mDict.get(pc),
@@ -103,7 +103,8 @@ def initUnits():
     pc: 1,
     ly: 3.26163344
     }
-  ly = Units("ly", "light-years", lyDict)
+  pc = Units("pc", "parsecs", pcDict)
+  
   lyDict = {
     km: 1/kmDict.get(ly),
     m: 1/mDict.get(ly),
@@ -111,20 +112,22 @@ def initUnits():
     pc: 1/pcDict.get(ly),
     ly: 1
     }
+  ly = Units("ly", "light-years", lyDict)
     
   # # # Temperature # # #
   # Empty dictionary for non linear conversions (should be handled in convert())
-  K = Units("K", "Kelvin", KDict)
   KDict = {}
+  K = Units("K", "Kelvin", KDict)
   
-  C = Units("C", "Celsius", CDict)
   CDict = {}
+  C = Units("C", "Celsius", CDict)
   
-  F = Units("F", "Fahrenheit", FDict)
   FDict = {}
+  F = Units("F", "Fahrenheit", FDict)
   
   # # # Magnitude # # #
   # There is only one unit of magnitude, so the dictionary should be empty.
-  mag = Units("mag", "magnitude", magDict)
   magDict = {}
+  mag = Units("mag", "magnitude", magDict)
+  
 
