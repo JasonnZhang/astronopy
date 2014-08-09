1e2801060e0ce5113c0fe9afd36a29feff411940
Amrit Rau <amritrau@users.noreply.github.com>
2014-08-08 23:24:13 -0700

    Update unitsGraph.py

--- unitsGraph.py
+++ unitsGraph.py
@@ -54,7 +54,7 @@ def find_path(graph, start, end, path=[]):
         path = path + [start]
         if start == end:
             return path
-        if graph.has_key(start) == false:
+        if graph.has_key(start) == 'false':
             return None
         for node in graph[start]:
             if node not in path:
